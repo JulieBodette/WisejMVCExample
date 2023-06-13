bd2f5847f7c58fb2748199912eb96944068171a1
JulieBodette <jr3python@gmail.com>
2023-06-13 16:23:09 -0400

    Update StudentController.cs

--- WisejMVCExample/WisejMVCExample/StudentController.cs
+++ WisejMVCExample/WisejMVCExample/StudentController.cs
@@ -1,6 +1,26 @@
-﻿namespace WisejMVCExample
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using Dapper;
+
+namespace WisejMVCExample
 {
 	public class StudentController
 	{
+		public static string CnnVal(string name)
+		{
+			return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+		}
+
+		public List<StudentModel> GetStudents()
+		{
+			using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(StudentController.CnnVal("Students")))
+			{
+				var output = connection.Query<StudentModel>("select * from Students").ToList();
+				return output;
+			}
+		}
 	}
 }
